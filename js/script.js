2f5fbea4ed08a2ef2caef74f2180af88b81842e9
Timon Riemslagh <timon.riemslagh@wijs.be>
2019-04-01 08:31:52 +0200

    Finetuning.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
     $('.slick-carousel').slick({
         dots: true,
         infinite: true,
-        arrows: true
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<div class="slick-prev slick-arrow"><i class="material-icons">arrow_back_ios</i></div>',
+        nextArrow: '<div class="slick-next slick-arrow"><i class="material-icons">arrow_forward_ios</i></div>',
     });
 });
